a92aa9a0516fef63693e9bc8c01b1aa61a35aca9
Mayank Tyagi <mayank.morta1209@gmail.com>
2024-07-31 14:35:19 +0530

    summer internship

---
+++ day4.py
@@ -0,0 +1,25 @@
+marks  ={'mohit':52,'rohit':56,'rocky':53,'jack':54 }
+# print(marks)
+# print (type(marks))
+# var['key']
+# print(marks['rocky'])
+# print(marks['mohit'])
+
+
+# print(marks['rohit'])
+# marks['rohit']=66
+#  marks['rahul']=100
+# print(marks.keys())
+# print(marks.values())
+
+
+# marks.pop('rohit')
+# print(marks)
+
+students = { "Name":['rahul','mohit','ritu','yesh'',jugnu',],
+               'marks':[52,62,63,45]         
+               'subject':"computer"}
+
+print(computer_marks['name'])
+print(computer_marks)
+print
